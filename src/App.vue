420dd8351a957880b8820d63162faa9aa73831b9
staszigzag <stas.zigzag@gmail.com>
2020-05-03 13:42:49 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <calendar-grid />
+  </div>
+</template>
+
+<script>
+import CalendarGrid from '@/components/CalendarGrid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalendarGrid
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 50px;
+}
+</style>
---
+++ src/components/CalendarGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="calendar">
+    Calendar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.calendar {
+  color: $color-item;
+}
+</style>
